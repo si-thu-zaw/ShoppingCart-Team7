5d4af426b2134b00f718a84e0ad53f9c0a2cea95
frozenpotatoes <52646745+frozenpotatoes@users.noreply.github.com>
2021-10-08 18:28:02 +0800

    Add project files.

---
+++ ShoppingCart-Team7/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShoppingCart_Team7.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
