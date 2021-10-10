022c622548c7b50fb24c7dc1dcc30bac446b668b
frozenpotatoes <52646745+frozenpotatoes@users.noreply.github.com>
2021-10-10 17:44:29 +0800

    Update 1: Database link with gallery page done Gallery prelim formatting done Layout changes in preparation for login system

--- ShoppingCart-Team7/Controllers/GalleryController.cs
+++ ShoppingCart-Team7/Controllers/GalleryController.cs
@@ -3,14 +3,40 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ShoppingCart_Team7.Models;
 
 namespace ShoppingCart_Team7.Controllers
 {
     public class GalleryController : Controller
     {
-        public IActionResult Index()
+        private DBContext dbContext;
+        
+        public GalleryController(DBContext dBContext)
         {
-            return View();
+            this.dbContext = dBContext;
         }
+
+        public IActionResult Index(string searchstring)
+        {
+            List<Product> products = new List<Product>();
+            if (searchstring == null)
+            {
+                searchstring = "";
+                products = dbContext.Products.ToList();
+            }
+            else
+            {
+                products = dbContext.Products.Where(x =>
+                    x.ProductName.Contains(searchstring) ||
+                    x.Description.Contains(searchstring)
+                    ).ToList();
+            }
+            ViewData["productlist"] = products;
+            ViewData["searchstring"] = searchstring;
+            return View();
+
+        }
+
+        
     }
 }
