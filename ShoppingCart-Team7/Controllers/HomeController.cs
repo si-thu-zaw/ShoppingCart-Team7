69deadeeda02b47117a474cf8ca239dd90c4a714
Tempolu <tempolulu@outlook.com>
2021-10-11 11:53:29 +0800

    Added image part and product detail part

--- ShoppingCart-Team7/Controllers/HomeController.cs
+++ ShoppingCart-Team7/Controllers/HomeController.cs
@@ -6,16 +6,16 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using ShoppingCart_Team7.Data;
 
 namespace ShoppingCart_Team7.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        public  DBContext dbContext;
+        public HomeController(DBContext dbContext)
         {
-            _logger = logger;
+            this.dbContext = dbContext;
         }
 
         public IActionResult Index()
@@ -32,6 +32,45 @@ namespace ShoppingCart_Team7.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult ProductDetail(string ProductId)
+        {
+            Product product = DetailData.GetAllDetails(ProductId);
+            ViewData["ProductId"] = ProductId;
+            ViewData["product"] = product;
+
+
+            List<Review> reviewList = dbContext.Reviews.ToList();
+            var ratings = reviewList.GroupBy(x => x.ProductId);
+            float arating = 0;
+            foreach (var grp in ratings)
+            {
+                if (grp.Key.ToString() == ProductId)
+                {
+                    Debug.WriteLine("okok");
+                    arating = grp.Average(x => x.Rating);
+                }
+                
+                Debug.WriteLine($"{ grp.Key} { grp.Average(x => x.Rating)}");
+            };
+
+            ViewData["rating"] = arating;
+            Debug.WriteLine(arating);
+
+            return View();
+
+            /*var iter =
+                from re in reviewList
+                where re.ProductId.Equals(ProductId)
+                select re.Rating;
+
+            foreach (var i in iter)
+                Debug.WriteLine(i.Rating);
+
+            /*var Ratings = AllReviews.GroupBy(x => x.ProductId)
+                                    .OrderByDescending(x => x.Average(x => x.Rating))
+                                    .Take(5);*/
         }
     }
 }
