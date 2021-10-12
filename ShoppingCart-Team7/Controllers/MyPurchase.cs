4bc8d74b42770456ad4b59a05beff9a8a8139849
Lina <82659876+Wanderstarz@users.noreply.github.com>
2021-10-13 01:55:03 +0800

    Update Purchase

--- ShoppingCart-Team7/Controllers/MyPurchase.cs
+++ ShoppingCart-Team7/Controllers/MyPurchase.cs
@@ -18,25 +18,82 @@ namespace ShoppingCart_Team7.Controllers
             this.dbContext = dbContext;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int id)
         {
-            List<Purchase> purchases = dbContext.Purchases.ToList();
-            List<Cart> carts = dbContext.Carts.ToList();
+            string username = "charles";
+            User Buyer = dbContext.Users.FirstOrDefault(u => u.UserName == username);
+            List<Purchase> purchases = dbContext.Purchases.Where(x => x.UserId == Buyer.Id).ToList();
             List<Product> products = dbContext.Products.ToList();
 
-            ViewData["purchases"] = purchases;
-            ViewData["carts"] = carts;
+            ViewData["purchases"] = Sort(purchases, id);
             ViewData["products"] = products;
-
-            foreach (Purchase p in purchases)
-            {
-
-                Cart c = carts.FirstOrDefault(x => x.ProductId == p.ProductId);
-            }
+            ViewData["searchStr"] = "";
 
             return View();
         }
 
-        
+        public List<Purchase> Sort(List<Purchase> p, int id)
+        {
+            DateTime d = DateTime.Today;
+
+            if (id == 2)
+            {
+                var purchases =
+                from pur in p
+                where pur.PurchaseDate.Month <= d.Month && pur.PurchaseDate.Month >= d.Month - 1
+                select pur;
+
+                List<Purchase> purchaseList = new List<Purchase>();
+
+                foreach (Purchase pc in purchases)
+                {
+                    purchaseList.Add(pc);
+                }
+                return purchaseList;
+            }
+            if (id == 3)
+            {
+                var purchases =
+                from pur in p
+                where pur.PurchaseDate.Year <= d.Year && pur.PurchaseDate.Year >= d.Year - 1
+                select pur;
+
+                List<Purchase> purchaseList = new List<Purchase>();
+
+                foreach (Purchase pc in purchases)
+                {
+                    purchaseList.Add(pc);
+                }
+                return purchaseList;
+            }
+
+            return p;
+
+        }
+
+        public IActionResult Search(int id, string searchStr)
+        {
+            if (searchStr == null)
+            {
+                searchStr = "";
+            }
+
+            string username = "charles";
+            User Buyer = dbContext.Users.FirstOrDefault(u => u.UserName == username);
+            List<Product> products = dbContext.Products.Where(x => x.ProductName.Contains(searchStr)).ToList();
+            List<Purchase> purchases = dbContext.Purchases.Where(x => x.UserId == Buyer.Id).ToList();
+
+            foreach (Product pdt in products)
+            {
+                purchases = dbContext.Purchases.Where(x => x.ProductId == pdt.Id).ToList();
+            }
+
+            ViewData["purchases"] = purchases;
+            ViewData["products"] = products;
+            ViewData["searchStr"] = searchStr;
+
+            return View("Index");
+        }
+
     }
 }
