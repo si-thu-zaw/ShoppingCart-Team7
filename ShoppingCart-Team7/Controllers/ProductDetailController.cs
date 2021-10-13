a13dd4f46ad4a1d3ef19add098a9c2463b355793
Tempoolu <ludantong77@gmail.com>
2021-10-13 09:33:52 +0800

    add recommendation to detail page

--- ShoppingCart-Team7/Controllers/ProductDetailController.cs
+++ ShoppingCart-Team7/Controllers/ProductDetailController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using ShoppingCart_Team7.Data;
 using ShoppingCart_Team7.Models;
 using System;
 using System.Collections.Generic;
@@ -14,7 +13,6 @@ namespace ShoppingCart_Team7.Controllers
     public class ProductDetailController : Controller
     {
         private DBContext dbContext;
-
         public ProductDetailController(DBContext dbContext)
         {
             this.dbContext = dbContext;
@@ -22,31 +20,33 @@ namespace ShoppingCart_Team7.Controllers
 
         public IActionResult Index(string ProductId)
         {
+            // use GetAllDetails function to get info from BD products table(Ado.NET)
             Product product = GetAllDetails(ProductId);
             ViewData["ProductId"] = ProductId;
             ViewData["product"] = product;
 
-
+            // get rating info from DB reviews table(LINQ)
             List<Review> reviewList = dbContext.Reviews.ToList();
             var ratings = reviewList.GroupBy(x => x.ProductId);
-            float arating = 0;
+            int arating = 0;
+            int arating_num = 0;
             foreach (var grp in ratings)
-            {
-                if (grp.Key.ToString() == ProductId)
+                if (grp.Key.ToString().ToUpper() == ProductId)
                 {
-                    Debug.WriteLine("okok");
-                    arating = grp.Average(x => x.Rating);
+                    arating = (int)Math.Round(grp.Average(x => x.Rating));
+                    arating_num = grp.Count();
                 }
 
-                Debug.WriteLine($"{ grp.Key} { grp.Average(x => x.Rating)}");
-            };
-
+            ViewData["rating_num"] = arating_num;
             ViewData["rating"] = arating;
-            Debug.WriteLine(arating);
+            ViewData["Recommendation"] = GetRecommendations(ProductId);
+
+            // ger review number from DB reviews table
 
             return View();
         }
 
+        // Get AllDetail funtion
         protected static readonly string connectionString = "Server=localhost;Database=ShoppingCartDB; Integrated Security=true";
         public static Product GetAllDetails(string ProductId)
         {
@@ -54,36 +54,93 @@ namespace ShoppingCart_Team7.Controllers
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 conn.Open();
-                string sql = @"select Products.ProductName, Products.Price, Products.Description, Products.ImageSrc, Products.Category from Products
+                string sql = @"select Products.Id, Products.ProductName, Products.Price, Products.Description, Products.ImageSrc, Products.Category from Products
                     where Products.Id = '" + ProductId + "'";
-
-                //string sql1 = @"select firstname from users where firstname = 'jack'";
 
                 Debug.WriteLine(sql);
                 SqlCommand cmd = new SqlCommand(sql, conn);
                 SqlDataReader reader = cmd.ExecuteReader();
                 if (reader.Read())
                 {
-                    Debug.WriteLine("okokok");
                     product = new Product()
                     {
                         ProductName = (string)reader["ProductName"],
                         Price = (float)reader["Price"],
                         ImageSrc = (string)reader["ImageSrc"],
                         Category = (string)reader["Category"],
-                        Description = (string)reader["Description"]
+                        Description = (string)reader["Description"],
+                        Id = (Guid)reader["Id"]
                     };
                 };
             }
             return product;
         }
 
-        public IActionResult Details(Guid ID)
+        // GetRecommendations function
+        public List<Product> GetRecommendations(string Id)
         {
-            Product product = dbContext.Products.FirstOrDefault(x => x.Id == ID);
-            ViewData["Product"] = product;
-            ViewData["Recommendation"] = GetRecommendations(ID);
-            return View();
+            Random r = new Random();
+            List<Product> Recommendations = new List<Product>();
+            List<Purchase> PastPurchases = new List<Purchase>();
+
+            int SelectionCount = 4; // Number of recommendations to return
+
+            // Find list of purchases with the same Product ID
+            List<Purchase> SalesWithSameProductId = dbContext.Purchases.Where(x => x.ProductId.ToString() == Id).ToList();
+
+            // If no one has bought this item before, update selectionCount so it won't return a recommendation list
+            if (SalesWithSameProductId.Count() == 0)
+            {
+                SelectionCount = 0;
+                return Recommendations;
+            }
+
+            // Get distinct users who bought the same book before
+            List<Guid> Users = (from purchase in SalesWithSameProductId
+                                select purchase.UserId).Distinct().ToList();
+
+            // Get all past purchases made by users who bought the same book
+            foreach (Guid user in Users)
+            {
+                List<Purchase> UserPurchase = dbContext.Purchases.Where(x => x.UserId == user && x.ProductId.ToString() != Id).ToList();
+
+                // Append list of Purchases to
+                PastPurchases.AddRange(UserPurchase);
+            }
+
+            // Get distinct Product Id from Past Purchases
+            List<Guid> ProductIds = (from pur in PastPurchases
+                                     select pur.ProductId).Distinct().ToList();
+
+            // Change SelectionCount according to ProductIds count if ProductIds count < 4
+            if (ProductIds.Count() < 4)
+                SelectionCount = ProductIds.Count();
+
+            // Generate recommendations
+            List<int> BookIndex = new List<int>();
+
+            // Choose n number of random books based on Selection count
+            for (int i = 0; i < SelectionCount; i++)
+            {
+                bool check = true;
+                while (check)
+                {
+                    int index = r.Next(ProductIds.Count());
+                    if (!BookIndex.Contains(index))
+                    {
+                        BookIndex.Add(index);
+                        check = false;
+                    }
+                }
+            }
+
+            // Add books into Recommendation List
+            foreach (int i in BookIndex)
+            {
+                Recommendations.Add(dbContext.Products.FirstOrDefault(x => x.Id == ProductIds[i]));
+            }
+
+            return Recommendations;
         }
     }
 }
