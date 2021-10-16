f69b540628bb40182b0787a9c054e3d46ca0ebdf
frozenpotatoes <52646745+si-thu-zaw@users.noreply.github.com>
2021-10-16 21:16:35 +0800

    Updated MyPurchases sort to grp.Key.PurchaseDate from DateTime.Parse(grp.Key.ToString().Substring(67, 17).Trim('}'))

--- ShoppingCart-Team7/Controllers/MyPurchase.cs
+++ ShoppingCart-Team7/Controllers/MyPurchase.cs
@@ -66,7 +66,7 @@ namespace ShoppingCart_Team7.Controllers
                                                   PID_PDATE = grp.Key.ToString(),
                                                   ActivationCodes = grp.Select(a => a.ActivationCode).ToList(),
                                                   Quantity = grp.Select(a => a.ActivationCode).Count(),
-                                                  Date = DateTime.Parse(grp.Key.ToString().Substring(67, 17).Trim('}'))
+                                                  Date = grp.Key.PurchaseDate
                                               }).ToList();
             }
 
