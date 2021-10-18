40e8621dbb6a6ba282c5b3376860e5fb23635e25
frozenpotatoes <52646745+si-thu-zaw@users.noreply.github.com>
2021-10-18 16:56:14 +0800

    Fixed MyPurchase search, replaced fixed username with cookie in Search

--- ShoppingCart-Team7/Controllers/MyPurchase.cs
+++ ShoppingCart-Team7/Controllers/MyPurchase.cs
@@ -229,7 +229,7 @@ namespace ShoppingCart_Team7.Controllers
                 searchStr = "";
             }
             string searchStrLower = searchStr.ToLower();
-            string username = "charles";
+            string username = Request.Cookies["Username"];
             User Buyer = dbContext.Users.FirstOrDefault(u => u.UserName == username);
 
             List<Product> products = dbContext.Products.Where(x => x.ProductName.ToLower().Contains(searchStrLower)).ToList();
@@ -253,7 +253,9 @@ namespace ShoppingCart_Team7.Controllers
                                                               {
                                                                   PID_PDATE = grp.Key.ToString(),
                                                                   ActivationCodes = grp.Select(a => a.ActivationCode).ToList(),
-                                                                  Quantity = grp.Select(a => a.ActivationCode).Count()
+                                                                  Quantity = grp.Select(a => a.ActivationCode).Count(),
+                                                                  Date = grp.Key.PurchaseDate,
+                                                                  PurchaseIDs = grp.Select(a => a.Id).ToList()
                                                               }).ToList();
 
             ViewData["codes"] = codes;
